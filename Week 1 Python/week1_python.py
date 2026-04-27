e6d65ad6f07f4f1e50acfe16aee7a14eeac13a85
sneakyturtle-png <paula.r.atkins@gmail.com>
2026-04-28 02:19:25 +1200

    Add Week 1 SQL & First Python

---
+++ Week 1 Python/week1_python.py
@@ -0,0 +1,20 @@
+name='Auckland'
+population=1657200
+gdp=92.5
+print(f'{name} has {population:,} people and a GDP of ${gdp} billion')
+
+name='Hawkes Bay'
+population=179700
+month='June'
+year=2025
+print(f'{name} as of {month} {year} has a population of {population} people')
+
+# Data types
+city_name='Auckland' # This is a string (text)
+daughter='Essence'
+d_age=15
+m_age=35
+name='Paula'
+d_school='Western Springs'
+studies='Data Analytics'
+print(f'My name is {name} and I live in {city_name} with my {d_age} year old daughter {daughter}. My daughter goes to {d_school} and I am studying {studies}')
